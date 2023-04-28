8e4095e611da06a000945075fec8c7d6f6f4d864
Frank Böker <boeker.frank@gmail.com>
2023-04-28 10:22:29 +0200

    WIP start

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,17 +1,44 @@
-import { places } from '../../../../lib/db.js';
+// import { places } from '../../../../lib/db.js';
 
-export default function handler(request, response) {
+import dbConnect from "../../../../db/connect";
+import Place from "../../../../db/models/Place";
+
+export default async function handler(request, response) {
+  await dbConnect();
+
   const { id } = request.query;
-
+  // console.log("api-id-id", id);
   if (!id) {
     return;
   }
 
-  const place = places.find((place) => place.id === id);
+  if (request.method === "GET") {
+    const place = await Place.findById(id);
 
-  if (!place) {
-    return response.status(404).json({ status: 'Not found' });
+    if (!place) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+
+    response.status(200).json(place);
   }
 
-  response.status(200).json(place);
+  if (request.method === "POST") {
+    try {
+      const newPlace = request.body;
+      console.log("PUT:", newPlace);
+      await Place.create(newPlace);
+      response.status(201).json({ status: "Place created!" });
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error: error.message });
+    }
+  }
+
+  // const place = places.find((place) => place.id === id);
+
+  // if (!place) {
+  //   return response.status(404).json({ status: 'Not found' });
+  // }
+
+  // response.status(200).json(place);
 }
--- pages/create.js
+++ pages/create.js
@@ -1,18 +1,37 @@
-import Link from 'next/link.js';
-import styled from 'styled-components';
-import { useRouter } from 'next/router';
-import Form from '../components/Form.js';
-import { StyledLink } from '../components/StyledLink.js';
+import Link from "next/link.js";
+import styled from "styled-components";
+import { useRouter } from "next/router";
+import Form from "../components/Form.js";
+import { StyledLink } from "../components/StyledLink.js";
+import { useSWRConfig } from "swr";
+import useSWRMutation from "swr/mutation";
 
 const StyledBackLink = styled(StyledLink)`
   justify-self: flex-start;
 `;
 
 export default function CreatePlacePage() {
-  const router = useRouter();
+  // const router = useRouter();
+  // const { id } = router.query;
+  // // console.log("pages-id:", { id });
+  // const { data, isLoading } = useSWRConfig(`/api/products/${id}`);
+
+  async function sendRequest(url, { arg }) {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(arg),
+    });
+    const { status } = await response.json();
+  }
+
+  const { trigger } = useSWRMutation("/api", sendRequest);
 
   function addPlace(place) {
-    console.log('Place added (but not really...)');
+    console.log("places", place);
+    trigger(place);
   }
 
   return (
@@ -21,7 +40,7 @@ export default function CreatePlacePage() {
       <Link href="/" passHref legacyBehavior>
         <StyledBackLink>back</StyledBackLink>
       </Link>
-      <Form onSubmit={addPlace} formName={'add-place'} />
+      <Form onSubmit={addPlace} formName={"add-place"} />
     </>
   );
 }
